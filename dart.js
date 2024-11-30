e6628800ef7f5c93a0bece0b50ecbcedae2a1695
Batuhan Bedir <batuhan.bedir133@gmail.com>
2024-11-30 18:42:47 +0300

    edit2

--- dart.js
+++ dart.js
@@ -3,16 +3,13 @@ let actionHistory = [];
 function toggleMark(cell, player) {
     const currentMark = cell.innerText;
     const newMark = currentMark === '' ? '/' : currentMark === '/' ? 'X' : currentMark === 'X' ? 'O' : 'O';
-    
     actionHistory.push({ cell, previousMark: currentMark, wasBlack: cell.classList.contains('black') });
     cell.innerText = newMark;
 
-    // Sadece 'O' olduğunda siyah arka plan ekle
     if (newMark === 'O') {
         cell.classList.add('black');
     } else {
         cell.classList.remove('black'); 
-        // Diğer durumlar için siyah rengi kaldır
     }
 }
 
@@ -21,7 +18,6 @@ function undoLastAction() {
         const lastAction = actionHistory.pop();
         lastAction.cell.innerText = lastAction.previousMark;
 
-        // Yeşil renk durumunu geri yükle
         if (lastAction.previousMark === 'O') {
             lastAction.cell.classList.add('black');
         } else {
@@ -34,7 +30,7 @@ function clearAll() {
     const markCells = document.querySelectorAll('.mark-cell');
     markCells.forEach(cell => {
         cell.innerText = '';
-        cell.classList.remove('black'); // Siyah rengi temizle
+        cell.classList.remove('black'); 
     });
     document.getElementById('player1Name').value = '';
     document.getElementById('player2Name').value = '';
